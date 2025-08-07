71042eb96959c12259e8f44d11a1aee5a2e3cc58
Chris Männik <chris.mannik@edu.turkuamk.fi>
2025-08-07 13:06:48 +0300

    feat: Very basic menu screen with nonfunctional button
    Very basic menu screen with red start button. This to visualize the colors of the project and the layout.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import MainMenu from "./scenes/MainMenu";
+
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white gap-4">
       <h1 className="text-4xl font-bold">Typeracer Game</h1>
+      <MainMenu />
     </div>
   );
 }
---
+++ frontend/src/scenes/MainMenu.tsx
@@ -0,0 +1,10 @@
+function MainMenu() {
+  return (
+    <div className="space-y-2">
+      <button className="mt-4 px-4 py-2 bg-red-600 text-white rounded hover:bg-red-800">
+        Start Game
+      </button>
+    </div>
+  );
+}
+export default MainMenu;
