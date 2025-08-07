0e7bd95034455434f34b7e796ea65dd83c225f2f
Chris Männik <chris.mannik@edu.turkuamk.fi>
2025-08-07 15:54:34 +0300

    feat: sign in and register button added, also functions to buttons
    Added Sign in and Register button and added to all buttons on site functions that they move to their dedicated site.

---
+++ frontend/src/scenes/signinForm.tsx
@@ -0,0 +1,8 @@
+function SignIn() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white gap-4">
+      <h1>This is where the sign in form opens</h1>
+    </div>
+  );
+}
+export default SignIn;
