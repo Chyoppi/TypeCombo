89e99e2a72b8ae1ee9242c5508f9bcd49d9307b4
Chris Männik <chris.mannik@edu.turkuamk.fi>
2025-08-07 13:32:43 +0300

    feat: framer-motion added for menuscreen animations
    framer-motion added to project and menu button added animations

--- frontend/src/scenes/MainMenu.tsx
+++ frontend/src/scenes/MainMenu.tsx
@@ -1,10 +1,18 @@
+import { motion } from "framer-motion";
+
 function MainMenu() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white gap-4 space-y-2">
       <h1 className="text-4xl font-bold">Typeracer Game</h1>
-      <button className="mt-4 px-4 py-2 bg-red-600 text-white rounded hover:bg-red-800">
+
+      <motion.button
+        initial={{ y: 50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1, ease: "easeOut" }}
+        className="mt-4 px-6 py-2 bg-red-600 text-white rounded hover:bg-red-800"
+      >
         Start Game
-      </button>
+      </motion.button>
     </div>
   );
 }
