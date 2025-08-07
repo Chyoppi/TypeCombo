dee02ff701fc4cbe5fb0e062c1653533062a39e6
Chris Männik <chris.mannik@edu.turkuamk.fi>
2025-08-07 13:43:43 +0300

    feat: Additional text/img added to menu
    Menu improved by adding img and additional text

--- frontend/src/scenes/MainMenu.tsx
+++ frontend/src/scenes/MainMenu.tsx
@@ -2,14 +2,20 @@ import { motion } from "framer-motion";
 
 function MainMenu() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white gap-4 space-y-2">
-      <h1 className="text-4xl font-bold">Typeracer Game</h1>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white gap-4">
+      <img
+        src="/typecombo_logo.png"
+        alt="TypeCombo Logo"
+        className="w-32 h-32"
+      />
+      <h1 className="text-4xl font-bold">TypeCombo</h1>
+      <p className="text-2xl">Game where only accuracy and speed matters</p>
 
       <motion.button
         initial={{ y: 50, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 1, ease: "easeOut" }}
-        className="mt-4 px-6 py-2 bg-red-600 text-white rounded hover:bg-red-800"
+        className="text-xl mt-4 px-6 py-2 bg-red-600 text-white rounded hover:bg-red-800"
       >
         Start Game
       </motion.button>
